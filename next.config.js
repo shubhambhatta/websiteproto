6c9e5880219edd5c5b9c4ff38caa5570fe689c2a
Shubham <shubhambhatta@gmail.com>
2023-12-11 18:37:29 +0100

    done hompage for website

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["images.images4us.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
